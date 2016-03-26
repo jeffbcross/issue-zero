6698f1529e1792cdef24279b41667dc52ffa0c58
Jeff Cross <middlefloor@gmail.com>
2016-03-25 20:20:11 -0700

    feat(Close): add basic material styling to Close component

--- src/client/app/issues/close/close.ts
+++ src/client/app/issues/close/close.ts
@@ -1,5 +1,12 @@
 import {Component} from 'angular2/core';
+import {RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
+import {MdToolbar} from '@angular2-material/toolbar';
 
-@Component({templateUrl : 'app/issues/close/close.html'})
+@Component({
+  templateUrl : 'app/issues/close/close.html',
+  directives : [ ROUTER_DIRECTIVES, MdToolbar ]
+})
 export class CloseComponent {
+  id: string;
+  constructor(params: RouteParams) { this.id = params.get('id'); }
 }
