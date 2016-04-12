57d90b58bde410189870e46cf7eb0946da6eccb2
Jeff Cross <middlefloor@gmail.com>
2016-04-11 18:07:07 -0700

    feat(login): add login component

--- src/client/app/login/login.ts
+++ src/client/app/login/login.ts
@@ -1,10 +1,31 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {AngularFire} from 'angularfire2';
+import {MdButton} from '@angular2-material/button';
 
 @Component({
-  template: '',
-  providers: [],
-  directives: []
+  styles : [ `
+button[md-raised-button] {
+  margin: 8px;
+}
+h3.headline {
+  margin: 8px;
+  font-size: 24px;
+  line-height: 32px;
+}
+  ` ],
+  template : `
+<div *ngIf="!(af.auth | async)">
+  <h3 class="headline">
+    Keep your Github issues tidy,<br>
+    and your users happy.
+  </h3>
+  <button md-raised-button color="primary" (click)="af.auth.login()">
+    Log in with Github
+  </button>
+</div>
+  `,
+  directives : [ MdButton ]
 })
 export class Login {
-  constructor() {}
+  constructor(public af: AngularFire) {}
 }
